ec0bca73f8c0510878a1a5e5dd1337e0e546ae1f
aakalewii <erlewiirobot@gmail.com>
2026-05-12 22:00:20 +0100

    use ValidatinException

--- api_usuarios_dsw/app/Http/Controllers/AuthController.php
+++ api_usuarios_dsw/app/Http/Controllers/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 use App\Enums\Rol;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
